4f729a0903053668f381b243078780676f164919
pane2004 <alexlu2000clash@gmail.com>
2023-07-20 12:33:39 -0400

    give space for scrollbar

--- pages/widgets/stock/[...slug].tsx
+++ pages/widgets/stock/[...slug].tsx
@@ -79,6 +79,7 @@ export default function StockWidget() {
           width={"100%"}
           height={"100%"}
           direction={"column"}
+          p={5}
         >
           {configs && configs.length > 0 &&
             configs.map((widget, i) => (

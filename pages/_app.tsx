8f73025d1339109de7a21697d924028c9165d385
pane2004 <alexlu2000clash@gmail.com>
2023-08-22 21:01:05 -0400

    fix prod build error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import theme from "./theme";
 import { GridItemStyle } from "@/components/landing/display";
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    // 200-600 is for gradient
+    brand: {
+      main: "#3F3E5E",
+      light: "#706D91",
+      bg: "#F9F7FF",
+      highlight: "#8186F1",
+      contrast: "#00C893",
+      200: "#7F527B",
+      300: "#C16781",
+      400: "#F38975",
+      500: "#FFBC66",
+      600: "#F9F871",
+    },
+  },
+  fonts: {
+    body: "sans-serif",
+    heading: "sans-serif",
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
